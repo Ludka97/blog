3cb0bb5f33e331f5de0959fbc7677f253e52ae99
Lyudmila Kachan <ludkakachan@gmail.com>
2022-08-30 18:49:21 +0300

    Add lesson_28

---
+++ shop/management/commands/oma_spiders.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from scrapy import signals
+from scrapy.signalmanager import dispatcher
+from shop.models import Product
+from shop.spiders import OmaSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+class Command(BaseCommand):
+    help = "Crawl OMA catalog"
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        def crawler_results(signal, sender, item, response, spider):
+            Product.objects.update_or_create(external_id=item["external_id"], defaults=item)
+        dispatcher.connect(crawler_results, signal=signals.item_scraped)
+
+        process = CrawlerProcess(get_project_settings())
+        process.crawl(OmaSpider)
+        process.start()
